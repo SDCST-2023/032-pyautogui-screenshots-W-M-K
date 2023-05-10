6fc53eeaa7c77f0c77e500a5da2e5bc4a7bf7312
W-M-K <124245448+W-M-K@users.noreply.github.com>
2023-05-10 11:20:26 -0700

    nice

--- assignment.py
+++ assignment.py
@@ -1,4 +1,5 @@
 #!python3
+from ctypes import c_int64
 import time 
 import pyautogui
 from PIL import Image
@@ -8,28 +9,36 @@ z = 1
 b = 0
 c = 0
 start = input("Press enter to start the program: ")
-print("Just sit back and relax, make sure you don't move your mouse. If you want to exit just move the mouse to the top left of the screen.")
+print("Just sit back and relax, make sure you don't move your mouse. If you want to exit just move the mouse to the top of the screen quickly.")
 pyautogui.PAUSE = 0.01
+pyautogui.click(724, 362)
 pyautogui.moveTo(1430, 200)
 pyautogui.drag(0, 130, duration=1)
 
 pyautogui.click(213, 388)
 
 while x==0:
-    
-    
     while y < 300:
         pyautogui.click(213, 388,)
         y = y + z
-   
-   
-   
     if y == 300:
+        c1 = pyautogui.locateOnScreen('Screenshot 2023-05-05 105620.png', confidence=0.5) 
+        pyautogui.click(c1) 
+        c2 = pyautogui.locateOnScreen('Screenshot 2023-05-05 105635.png', confidence=0.5) 
+        pyautogui.click(c2)
+        c3 = pyautogui.locateOnScreen('Screenshot 2023-05-05 105712.png', confidence=0.5)
+        pyautogui.click(c3)
+        c4 = pyautogui.locateOnScreen('Screenshot 2023-05-10 111055.png', confidence=0.5)
+        pyautogui.click(c4) 
+        c5 = pyautogui.locateOnScreen('Screenshot 2023-05-10 111114.png', confidence=0.5)
+        pyautogui.click(c5) 
+        c6 = pyautogui.locateOnScreen('Screenshot 2023-05-10 111130.png', confidence=0.5)
+        pyautogui.click(c6) 
+        c7 = pyautogui.locateOnScreen('Screenshot 2023-05-10 111144.png', confidence=0.5)
+        pyautogui.click(c7)
+        c8 = pyautogui.locateOnScreen('Screenshot 2023-05-10 111157.png', confidence=0.5)
+        pyautogui.click(c8)  
         
-        pyautogui.locateOnScreen('Screenshot 2023-05-05 105620.png', confidence=0.4) 
-        pyautogui.click(x=pyautogui.center(pyautogui.locateOnScreen('Screenshot 2023-05-05 105620.png', confidence=0.4)).x, y=pyautogui.center(pyautogui.locateOnScreen('Screenshot 2023-05-05 105620.png', confidence=0.4)).y )
-        
-
         pyautogui.click(1154, 169) 
         pyautogui.click(1283, 840, 3)   
         pyautogui.click(1271, 790, 3)
